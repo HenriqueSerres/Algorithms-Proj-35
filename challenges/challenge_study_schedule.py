3f8752494d3da1d4e55235ad640723760684758c
HenriqueSerres <henriqueserres@gmail.com>
2022-10-10 16:06:30 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count_students = 0
+    try:
+        for get_in, get_out in permanence_period:
+            if get_in <= target_time and get_out >= target_time:
+                count_students += 1
+    except TypeError or ValueError:
+        return None
+    return count_students
